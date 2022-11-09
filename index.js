cc5da5a6fedc885a138120cce186170c55a42fed
Angel Rodriguez <angelrodri@hotmail.com>
2022-11-09 13:39:07 -0800

    added p and to save entries

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 let count = 0;
 
 function increment() {
-    count++
+    count += 1
     countEl.innerText = count;
 }
 
 function save() {
-    console.log(count);
+    let savedCount = count + " - ";
+    saveEl.innerText += savedCount;
 }
